b081e279b3d701ad08daab77ad96777d78086f31
Eduardo5011 <eduardo.rosado5011@gmail.com>
2024-06-13 21:01:51 -0400

    updated

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import React from "react";
+
+const url =
+  "";
 
 const Gallery = () => {
-  return (
-    <div>Gallery</div>
-  )
-}
 
-export default Gallery
+  const response = useQuery({
+    queryKey: ["images"],
+    queryFn: async () => {
+      const result = await axios.get(url)
+
+      return result.data
+    }
+  })
+  console.log(response)
+
+  return <div>Gallery</div>;
+};
+
+export default Gallery;
