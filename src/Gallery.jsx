b9decebace06b232d69cdc8dfd5b782bbebf31b8
Eduardo5011 <eduardo.rosado5011@gmail.com>
2024-06-20 22:01:35 -0400

    updated

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -6,18 +6,46 @@ const url =
   "";
 
 const Gallery = () => {
-
   const response = useQuery({
     queryKey: ["images"],
     queryFn: async () => {
-      const result = await axios.get(url)
+      const result = await axios.get(url);
 
-      return result.data
-    }
-  })
-  console.log(response)
+      return result.data;
+    },
+  });
+  if (response.isLoading) {
+    return (
+      <section className="image-container">
+        <h4>Loading...</h4>
+      </section>
+    );
+  }
+  if (response.isError) {
+    return (
+      <section className="image-container">
+        <h4>There was an error...</h4>
+      </section>
+    );
+  }
 
-  return <div>Gallery</div>;
+  const results = response.data.results;
+  if (results.length < 1) {
+    return (
+      <section className="image-container">
+        <h4>No results found...</h4>
+      </section>
+    );
+  }
+
+  return (
+    <section className="image-container">
+      {results.map((item) => {
+        const url = item?.urls?.regular;
+        return <img src={url} key={item.id} alt={item.alt_description} />;
+      })}
+    </section>
+  );
 };
 
 export default Gallery;
