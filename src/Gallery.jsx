f45724e1fa31cd80dcaf849ab4d839ef5c16019a
Eduardo5011 <eduardo.rosado5011@gmail.com>
2024-06-27 20:34:29 -0400

    updated

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,15 +1,19 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import React from "react";
+import { useGlobalContext } from "./context";
 
 const url =
-  "";
+  `https://api.unsplash.com/search/photos?client_id=${import.meta.env.VITE_API_KEY}`;
+  console.log(import.meta.env.VITE_API_KEY)
 
 const Gallery = () => {
+  const { searchTerm } = useGlobalContext();
+
   const response = useQuery({
-    queryKey: ["images"],
+    queryKey: ["images", searchTerm],
     queryFn: async () => {
-      const result = await axios.get(url);
+      const result = await axios.get(`${url}&query=${searchTerm}`);
 
       return result.data;
     },
